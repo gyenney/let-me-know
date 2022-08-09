36b0bd291f6ac7abb705d7d03b1a5cbb68d06ee9
Sean Siddens <seansiddens@gmail.com>
2022-08-09 15:01:00 -0700

    Added storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,15 @@
+import { Table } from "@serverless-stack/resources";
+
+export function StorageStack({ stack, app }) {
+    // TODO: What should the name be? 
+    const table = new Table(stack, "Notepads", {
+        fields: {
+            notepadId,
+            msgTimestamp,
+        },
+        primaryIndex: { partitionKey: "notepadId", sortKey: "msgTimestampt" },
+    });
+
+
+    return { table };
+}
