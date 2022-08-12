a31366d83780b6e82f771d7780a1ae73e2c006fb
Sean Siddens <seansiddens@gmail.com>
2022-08-11 22:09:28 -0700

    NotFound route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Links() {
     // Renders the first matching route that is defined.
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
