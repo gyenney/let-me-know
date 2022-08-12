7c7f61a62c4cb1bd24b79bde87a48473f3222576
Sean Siddens <seansiddens@gmail.com>
2022-08-11 22:12:40 -0700

    lib stuff

---
+++ frontend/src/lib/contextLib.js
@@ -0,0 +1,8 @@
+// Context for entire app that all containers will use.
+import { useContext, createContext } from "react";
+
+export const AppContext = createContext(null);
+
+export function useAppContext() {
+    return useContext(AppContext);
+}
