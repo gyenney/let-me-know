1bd6f38308ba7da44a3f0ddd19e2489ab6ef8252
Sean Siddens <seansiddens@gmail.com>
2022-08-11 22:02:49 -0700

    Wrapped navbar links in link containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import "./App.css";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
+import "./App.css";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          let me know
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            let me know
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link href="/signup">Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link href="/login">Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
