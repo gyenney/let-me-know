83e7fc004c473d08758d45b5475e4f26693f6416
Sean Siddens <seansiddens@gmail.com>
2022-08-30 16:10:09 -0700

    Forgot an exclamation point

--- frontend/src/components/AuthenticatedRoute.js
+++ frontend/src/components/AuthenticatedRoute.js
@@ -3,11 +3,11 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAppContext } from "../lib/contextLib";
 
-export default function AuthenticatedRoute({ children }) {
+export default function AuthentciatedRoute({ children }) {
     const { pathname, search } = useLocation();
     const { isAuthenticated } = useAppContext();
 
-    if (isAuthenticated) {
+    if (!isAuthenticated) {
         // Redirect to login page if user is not logged in.
         // We pass the current location to the login page so we can redirect back after login.
         return <Navigate to={`/login?redirect=${pathname}${search}`} />;
