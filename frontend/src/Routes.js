bb3f390ca54cb3ae97e3b232f935a64e777f63d9
Sean Siddens <seansiddens@gmail.com>
2022-08-30 07:37:27 -0700

    Added signup route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import Messages from "./containers/Messages";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup"
 
 export default function Links() {
     // Renders the first matching route that is defined.
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/signup"
+                element={
+                    <UnauthenticatedRoute>
+                        <Signup />
+                    </UnauthenticatedRoute>
+                }
+            />
             <Route path="/notepads/:id" element={<Messages />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
