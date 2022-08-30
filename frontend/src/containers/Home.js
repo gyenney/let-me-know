a9d15959b75a09b248dd34bcb002eeed0ad78387
Sean Siddens <seansiddens@gmail.com>
2022-08-30 11:46:09 -0700

    Created new storage stack

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -2,11 +2,13 @@
 import React from "react";
 import "./Home.css";
 
+// TODO: Have a search feature or "New Notepad" feature to the home page.
+
 export default function Home() {
     return (
         <div className="Home">
             <div className="lander">
-                <h1>let me know</h1>
+                <h1>Let Me Know</h1>
             </div>
         </div>
     )
--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -2,13 +2,30 @@ import { Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
     // TODO: Have two tables, one for storing messages and one for notepads.
-    const table = new Table(stack, "Notepads", {
+    const messageTable = new Table(stack, "Messages", {
         fields: {
             notepadId: "string",
             msgTimestamp: "number",
         },
+        // Fetch all messages within a given notepad, sorted by the timestamp.
         primaryIndex: { partitionKey: "notepadId", sortKey: "msgTimestamp" },
     });
 
-    return { table };
+    const notepadTable = new Table(stack, "Notepads", {
+        fields: {
+            notepadId: "string",      // Unique UUID for identifying a notepad
+            mainIdentifier: "string", // A "name" for the notepad; used for querying.
+            createdAt: "number",      // Time of when the notepad was created.
+        },
+        primaryIndex: { partitionKey: "notepadId" },
+        globalIndexes: {
+            // Query notepads with based on main identifier.
+            // TOOD: If we only project the key, and have the id as the sort key, might this increase
+            // performance in some way? See: https://docs.sst.dev/constructs/Table#projection-1
+            mainIdentifierIndex: { partitionKey: "mainIdentifier", sortKey: "createdAt" },
+            // TODO: Connection engine can utilize other identifiers to make queries.
+        }
+    });
+
+    return { messageTable, notepadTable };
 }
