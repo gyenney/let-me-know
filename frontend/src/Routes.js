5e66c42fe02ccd974e7a6f3402e2c6b6f573ca76
Sean Siddens <seansiddens@gmail.com>
2022-08-30 08:01:53 -0700

    Login route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Home from "./containers/Home";
 import Messages from "./containers/Messages";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup"
+import Login from "./containers/Login";
 
 export default function Links() {
     // Renders the first matching route that is defined.
@@ -18,7 +20,20 @@ export default function Links() {
                     </UnauthenticatedRoute>
                 }
             />
-            <Route path="/notepads/:id" element={<Messages />} />
+            <Route path="/login"
+                element={
+                    <UnauthenticatedRoute>
+                        <Login />
+                    </UnauthenticatedRoute>
+                }
+            />
+            <Route path="/notepads/:id" element={
+                // TODO: For just seeing messages, user probably doesn't need to be 
+                // logged-in.
+                <AuthenticatedRoute>
+                    <Messages />
+                </AuthenticatedRoute>
+            } />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
